9aa4ee16c0466fcf3190865d32689b3e922014cd
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-11 12:37:20 +0900

    diff pages using datetime

--- build.sbt
+++ build.sbt
@@ -12,4 +12,5 @@ libraryDependencies ++= Seq(
   "com.googlecode" % "difflib" % "1.2.1" from "https://java-diff-utils.googlecode.com/files/diffutils-1.2.1.jar"
 , "net.databinder.dispatch" %% "dispatch-core" % "0.11.0"
 , "org.slf4j" % "slf4j-simple" % "1.6.4"
+, "com.github.nscala-time" %% "nscala-time" % "1.2.0"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,9 @@
 import difflib._
 import scala.collection.JavaConversions._
+import scala.io._
+import java.io.PrintWriter
+import java.nio.charset.CodingErrorAction
+import com.github.nscala_time.time.Imports._
 import diffutils.App._
 
 object getPageUrlConst {
@@ -8,22 +12,38 @@ object getPageUrlConst {
 
 object Application {
 
-  def run = {
+  def run: String = {
     import dispatch._, Defaults._
+    implicit val codec = Codec("UTF-8")
+    codec.onMalformedInput(CodingErrorAction.REPLACE)
+    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
     val req = Http(host(getPageUrlConst.pageUrl).secure OK as.String).apply()
     Http.shutdown
-    req
+    val src = Source.fromBytes(req.getBytes(), "UTF-8").mkString
+    src
+  }
+
+  def writeFile(filename: String, contents: String) = {
+    val out = new PrintWriter(filename)
+    out.println(contents)
+    out.close
   }
 
   def main(args: Array[String]) {
+    val date = new DateTime
+    val yesterday = date - 1.day
+    val fdate = date.toString("yyyyMMdd")
+    val ydate = yesterday.toString("yyyyMMdd")
     val downloadDir = "tmp/"
-    val original = fileToLines(downloadDir ++ "test.txt")
-    val revised = fileToLines(downloadDir ++ "test2.txt")
+
+    writeFile(downloadDir ++ s"piyo_$fdate.html", run)
+
+    val original = fileToLines(downloadDir ++ s"piyo_$ydate.html")
+    val revised = fileToLines(downloadDir ++ s"piyo_$fdate.html")
 
     val patch = DiffUtils.diff(original, revised)
 
     for (out <- patch.getDeltas)
       println(out)
-    println(run)
   }
 }
